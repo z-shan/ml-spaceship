d5cbd8c0a057abae75e78303b604ce05fff32bbf
z-shan <zeeshan2ahmed@gmail.com>
2017-06-17 20:32:33 +0300

    upgrade react and react-dom

--- src/routes.js
+++ src/routes.js
@@ -10,7 +10,6 @@ var NotFoundRoute = Router.NotFoundRoute;
 var routes = (
     <Route name="app" path="/" handler={require('./components/app')}>
         <DefaultRoute handler={require('./components/homePage')} />
-        <Route name="authors" handler={require('./components/authors/authorPage')} />
         <Route name="shop" handler={require('./components/shop/inventoryPage')} />
         <Route name="about" handler={require('./components/about/aboutPage')} />
         <NotFoundRoute handler={require('./components/notFoundPage')} />
